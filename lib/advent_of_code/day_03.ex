b83594846f19d98eac66c39787b70f2488c7e2b8
RafaAudibert <rafaeelaudibert@gmail.com>
2020-12-03 05:33:44 -0300

    :rocket: Added day 3

--- lib/advent_of_code/day_03.ex
+++ lib/advent_of_code/day_03.ex
@@ -1,13 +1,35 @@
 import AdventOfCode.Helpers.Input
 
 defmodule AdventOfCode.Day03 do
-  @spec part1(String.t()) :: integer
-  def part1(test_filename) do
-    input = read_integers(test_filename)
+
+  @spec compute_trees([[String.t()]], {non_neg_integer, non_neg_integer}) :: non_neg_integer
+  defp compute_trees(matrix, {slope_x, slope_y}) do
+    size = Enum.count(matrix)
+    size_inside = Enum.at(matrix, 0) |> Enum.count()
+
+    Stream.take_every(0..size-1, slope_y)
+      |> Enum.with_index()
+      |> Enum.reduce(0, fn {x, idx}, acc ->
+        if (Enum.at(matrix, x) |> Enum.at(rem(slope_x * idx, size_inside))) == "#",
+          do: acc + 1,
+          else: acc
+      end)
   end
 
-  @spec part2(String.t()) :: integer
+  @spec part1(String.t()) :: non_neg_integer
+  def part1(test_filename) do
+    read_lines(test_filename)
+      |> Enum.map(&String.graphemes/1)
+      |> compute_trees({3, 1})
+  end
+
+  @spec part2(String.t()) :: non_neg_integer
   def part2(test_filename) do
-    input = read_integers(test_filename)
+    matrix = read_lines(test_filename) |> Enum.map(&String.graphemes/1)
+    slopes = [{1, 1}, {3, 1}, {5, 1}, {7, 1}, {1, 2}]
+
+    slopes
+      |> Enum.map(&compute_trees(matrix, &1))
+      |> Enum.reduce(1, fn x, acc -> x * acc end)
   end
 end
--- test/advent_of_code/day_03_test.exs
+++ test/advent_of_code/day_03_test.exs
@@ -1,15 +1,13 @@
 defmodule AdventOfCode.Day03Test do
   use ExUnit.Case
   
-  @tag :skip
   test "part1" do
     result = AdventOfCode.Day03.part1(AdventOfCode.Helpers.File.get_path(3))
-    assert result
+    assert result == 218
   end
 
-  @tag :skip
   test "part2" do
     result = AdventOfCode.Day03.part2(AdventOfCode.Helpers.File.get_path(3))
-    assert result
+    assert result == 3847183340
   end
 end
