722d219444ce97fd411996e4431991552a9f27de
RafaAudibert <rafaeelaudibert@gmail.com>
2020-12-05 05:28:19 -0300

    :rocket: Add day 5

--- lib/advent_of_code/day_05.ex
+++ lib/advent_of_code/day_05.ex
@@ -1,13 +1,29 @@
 import AdventOfCode.Helpers.Input
 
 defmodule AdventOfCode.Day05 do
+  use Bitwise
+
+  defp parse_to_binary(items) do
+    Enum.reduce(items, 0, fn y, acc -> (acc <<< 1) + if y in ~w[B R], do: 1, else: 0 end)
+  end
+
+  @spec get_input(String.t()) :: non_neg_integer()
+  defp get_input(test_filename) do
+    read_lines(test_filename)
+      |> Enum.map(&String.graphemes/1)
+      |> Enum.map(& &1 |> parse_to_binary())
+  end
+
   @spec part1(String.t()) :: non_neg_integer()
   def part1(test_filename) do
-    input = read_integers(test_filename)
+    get_input(test_filename) |> Enum.max()
   end
 
   @spec part2(String.t()) :: non_neg_integer()
   def part2(test_filename) do
-    input = read_integers(test_filename)
+    numbers = get_input(test_filename) |> MapSet.new()
+    
+    Stream.iterate(1, & &1 + 1)
+      |> Enum.find(& &1 - 1 in numbers and &1 + 1 in numbers and &1 not in numbers)
   end
 end
--- test/advent_of_code/day_05_test.exs
+++ test/advent_of_code/day_05_test.exs
@@ -1,15 +1,13 @@
 defmodule AdventOfCode.Day05Test do
   use ExUnit.Case
   
-  @tag :skip
   test "part1" do
     result = AdventOfCode.Day05.part1(AdventOfCode.Helpers.File.get_path(5))
-    assert result
+    assert result === 835
   end
 
-  @tag :skip
   test "part2" do
     result = AdventOfCode.Day05.part2(AdventOfCode.Helpers.File.get_path(5))
-    assert result
+    assert result === 649
   end
 end
