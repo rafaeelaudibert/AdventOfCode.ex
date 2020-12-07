61f0a58ab6ec34dbe0a2ade4a6c1cecb3eb7f920
RafaAudibert <rafaeelaudibert@gmail.com>
2020-12-07 02:03:12 -0300

    :rocket: Add day 6

--- test/advent_of_code/day_06_test.exs
+++ test/advent_of_code/day_06_test.exs
@@ -1,15 +1,13 @@
 defmodule AdventOfCode.Day06Test do
   use ExUnit.Case
   
-  @tag :skip
   test "part1" do
     result = AdventOfCode.Day06.part1(AdventOfCode.Helpers.File.get_path(6))
-    assert result
+    assert result == 6778
   end
 
-  @tag :skip
   test "part2" do
     result = AdventOfCode.Day06.part2(AdventOfCode.Helpers.File.get_path(6))
-    assert result
+    assert result == 3406
   end
 end
