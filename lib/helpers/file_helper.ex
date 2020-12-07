42a20eb1d6d1b723a2b8120aca3db50b3ada1fab
RafaAudibert <rafaeelaudibert@gmail.com>
2020-12-07 06:58:11 -0300

    :memo: Add docs on helpers

--- lib/helpers/file_helper.ex
+++ lib/helpers/file_helper.ex
@@ -1,7 +1,14 @@
 defmodule AdventOfCode.Helpers.File do
-    def get_path(day) when is_integer(day), do: get_path(Integer.to_string(day))
-    def get_path(day) when is_binary(day) do
-        padded_day = String.pad_leading(day, 2, "0")
-        Path.join(File.cwd!, "input/input#{padded_day}.txt")
-    end
+  @spec get_path(binary | integer) :: binary
+  @doc """
+      Returns the AOC input file, for the day passed in
+      If it is an integer, it tranforms in an String.t()
+      It also properly applies the padding for days with only 1 digit.
+  """
+  def get_path(day) when is_integer(day), do: get_path(Integer.to_string(day))
+
+  def get_path(day) when is_binary(day) do
+    padded_day = String.pad_leading(day, 2, "0")
+    Path.join(File.cwd!(), "input/input#{padded_day}.txt")
+  end
 end
--- lib/helpers/input_helper.ex
+++ lib/helpers/input_helper.ex
@@ -1,21 +1,52 @@
 defmodule AdventOfCode.Helpers.Input do
-    def read_integer(filename) do
-        read_integers(filename) |> hd()
-    end
+  @spec read_integer(String.t()) :: integer()
+  @doc """
+    Given a filename, reads the first integer in the file
+  """
+  def read_integer(filename) do
+    read_integers(filename) |> hd()
+  end
 
-    def read_integers(filename) do
-        read_lines(filename) |> Enum.map(&String.to_integer/1)
-    end
+  @spec read_integers(String.t()) :: [integer()]
+  @doc """
+    Given a filename, reads every integer in the file
+  """
+  def read_integers(filename) do
+    read_lines(filename) |> Enum.map(&String.to_integer/1)
+  end
 
-    def read_chunked_lines(filename, separator \\ "") do
-        read_lines(filename)
-            |> Enum.chunk_by(& &1 == separator)
-            |> Enum.filter(& Enum.at(&1, 0) != separator)
-    end
+  @spec read_chunked_lines(String.t(), String.t()) :: [[String.t()]]
+  @doc """
+    Given a filename, reads every line in the file, and chunks them
+    by the separator, with default as an empty line
 
-    def read_lines(filename) do
-        with {:ok, file} <- File.read(filename),
-            do: file |> String.replace("\r", "") |> String.split("\n")
-    end
+    ## Examples:
 
+    For the file with the following content:
+
+        abc
+        def
+        fhi
+
+        abc
+        def
+
+    Returns `[["abc", "def", "fhi"] ["abc", "def"]]`
+  """
+  def read_chunked_lines(filename, separator \\ "") do
+    read_lines(filename)
+    |> Enum.chunk_by(&(&1 == separator))
+    |> Enum.filter(&(Enum.at(&1, 0) != separator))
+  end
+
+  @spec read_lines(String.t()) :: [String.t()]
+  @doc """
+    Given a filename, reads every line in the file, and returns
+    a list of strings, according to each of the file lines
+    A line is counted as separated by a `\\n` character
+  """
+  def read_lines(filename) do
+    with {:ok, file} <- File.read(filename),
+         do: file |> String.replace("\r", "") |> String.split("\n")
+  end
 end
