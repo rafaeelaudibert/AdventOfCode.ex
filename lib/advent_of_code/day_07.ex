a9d92da974a64958ae3b2517e6ed66147943d030
RafaAudibert <rafaeelaudibert@gmail.com>
2020-12-07 06:45:02 -0300

    :rocket: Added day7 code

--- lib/advent_of_code/day_07.ex
+++ lib/advent_of_code/day_07.ex
@@ -1,13 +1,77 @@
 import AdventOfCode.Helpers.Input
 
 defmodule AdventOfCode.Day07 do
+  @type bag :: [{integer(), String.t()}]
+  @type graph :: %{String.t() => [String.t()]}
+
+  @spec parse_bags_string(String.t()) :: {String.t(), [bag()]}
+  def parse_bags_string(bags_string) do
+    [_ | [bag_color, other_colors]] = Regex.run(~r/(.+) bags contain (.+)\./U, bags_string)
+
+    other_colors =
+      String.split(other_colors, ", ")
+      |> Enum.map(fn color_string ->
+        String.replace(color_string, ~r/ bags?/, "")
+        |> String.split(" ", parts: 2)
+        |> List.to_tuple()
+      end)
+      |> Enum.filter(fn {number, _} -> number != "no" end)
+      |> Enum.map(fn {number, text} -> {Integer.parse(number) |> elem(0), text} end)
+
+    {bag_color, other_colors}
+  end
+
+  @spec graph_size(graph, String.t()) :: non_neg_integer()
+  def graph_size(graph, start) do
+    Stream.unfold([{0, start}], fn
+      [] -> nil
+      [{_, head} | tail] -> {head, tail ++ (graph[head] || [])}
+    end)
+    |> Enum.uniq()
+    |> length()
+    |> (&(&1 - 1)).()
+  end
+
+  @doc """
+    This builds a reversed graph, according to the entry.
+
+    Returns a new graph with the edges reversed, but keeping the same weights.
+
+    ## Examples
+        iex> reverse_graph([{"abc", [{"2", "abcde"}]}])
+        %{"abcde" => ["abc"]}
+  """
+  @spec reverse_graph(graph()) :: graph()
+  def reverse_graph(graph) do
+    graph
+    |> Enum.reduce(%{}, fn {name, items}, graph ->
+      Enum.reduce(items, graph, fn {count, edge}, graph ->
+        Map.update(graph, edge, [{count, name}], &[{count, name} | &1])
+      end)
+    end)
+  end
+
+  @spec count_bags(graph, String.t()) :: non_neg_integer()
+  def count_bags(graph, start) do
+    Enum.reduce(graph[start] || [], 0, fn {count, name}, acc ->
+      acc + count + count * count_bags(graph, name)
+    end)
+  end
+
   @spec part1(String.t()) :: non_neg_integer()
   def part1(test_filename) do
-    input = read_integers(test_filename)
+    read_lines(test_filename)
+    |> Enum.map(&parse_bags_string/1)
+    |> Map.new()
+    |> reverse_graph()
+    |> graph_size("shiny gold")
   end
 
   @spec part2(String.t()) :: non_neg_integer()
   def part2(test_filename) do
-    input = read_integers(test_filename)
+    read_lines(test_filename)
+    |> Enum.map(&parse_bags_string/1)
+    |> Map.new()
+    |> count_bags("shiny gold")
   end
 end
--- test/advent_of_code/day_07_test.exs
+++ test/advent_of_code/day_07_test.exs
@@ -1,15 +1,13 @@
 defmodule AdventOfCode.Day07Test do
   use ExUnit.Case
-  
-  @tag :skip
+
   test "part1" do
     result = AdventOfCode.Day07.part1(AdventOfCode.Helpers.File.get_path(7))
-    assert result
+    assert result == 248
   end
 
-  @tag :skip
   test "part2" do
     result = AdventOfCode.Day07.part2(AdventOfCode.Helpers.File.get_path(7))
-    assert result
+    assert result == 57281
   end
 end
