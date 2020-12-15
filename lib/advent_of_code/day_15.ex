0ceb07dcab2e9af8d5baf9624384a2a92a7993f1
RafaAudibert <rafaeelaudibert@gmail.com>
2020-12-15 02:53:03 -0300

    :rocket: Add day 15

--- lib/advent_of_code/day_15.ex
+++ lib/advent_of_code/day_15.ex
@@ -1,13 +1,45 @@
 import AdventOfCode.Helpers.Input
 
 defmodule AdventOfCode.Day15 do
-  @spec part1(String.t()) :: non_neg_integer()
-  def part1(test_filename) do
-    input = read_integers(test_filename)
+  @spec solve(non_neg_integer(), non_neg_integer(), Map.t(), non_neg_integer()) ::
+          non_neg_integer()
+  defp solve(curr_index, last_number, _, limit)
+       when curr_index == limit + 1,
+       do: last_number
+
+  defp solve(curr_index, last_number, map, limit) do
+    # We use the default as list_size, so that we don't need an "if"
+    # This is because when the number hasn't being used we have the following:
+    # list_size - last_index = list_size - list_size = 0
+    list_size = curr_index - 1
+    last_index = Map.get(map, last_number, list_size)
+
+    new_number = list_size - last_index
+    map = Map.put(map, last_number, list_size)
+    solve(curr_index + 1, new_number, map, limit)
   end
 
-  @spec part2(String.t()) :: non_neg_integer()
-  def part2(test_filename) do
-    input = read_integers(test_filename)
+  @spec solve(String.t(), non_neg_integer()) :: non_neg_integer()
+  defp solve(filename, limit) do
+    input =
+      read_lines(filename)
+      |> Enum.at(0)
+      |> String.split(",")
+      |> Enum.map(&(&1 |> Integer.parse() |> elem(0)))
+
+    solve(
+      length(input) + 1,
+      List.last(input),
+      Enum.with_index(input)
+      |> Enum.map(fn {val, idx} -> {val, idx + 1} end)
+      |> Map.new(),
+      limit
+    )
   end
+
+  @spec part1(String.t()) :: non_neg_integer()
+  def part1(test_filename), do: solve(test_filename, 2020)
+
+  @spec part2(String.t()) :: non_neg_integer()
+  def part2(test_filename), do: solve(test_filename, 30_000_000)
 end
--- test/advent_of_code/day_15_test.exs
+++ test/advent_of_code/day_15_test.exs
@@ -1,15 +1,14 @@
 defmodule AdventOfCode.Day15Test do
   use ExUnit.Case
-  
-  @tag :skip
+
   test "part1" do
     result = AdventOfCode.Day15.part1(AdventOfCode.Helpers.File.get_path(15))
-    assert result
+    assert result == 468
   end
 
-  @tag :skip
+  @tag :too_slow
   test "part2" do
     result = AdventOfCode.Day15.part2(AdventOfCode.Helpers.File.get_path(15))
-    assert result
+    assert result == 1_801_753
   end
 end
