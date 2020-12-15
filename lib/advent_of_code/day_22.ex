67981b075f88203ab9fed26fc924618df882762d
RafaAudibert <rafaeelaudibert@gmail.com>
2020-12-15 02:55:38 -0300

    :recycle: Remove ExDialyzer warnings

--- lib/advent_of_code/day_22.ex
+++ lib/advent_of_code/day_22.ex
@@ -3,11 +3,13 @@ import AdventOfCode.Helpers.Input
 defmodule AdventOfCode.Day22 do
   @spec part1(String.t()) :: non_neg_integer()
   def part1(filename) do
-    input = read_integers(filename)
+    _input = read_integers(filename)
+    0
   end
 
   @spec part2(String.t()) :: non_neg_integer()
   def part2(filename) do
-    input = read_integers(filename)
+    _input = read_integers(filename)
+    0
   end
 end
