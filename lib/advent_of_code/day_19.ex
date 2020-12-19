a1dcb35690789145289db3ce8f88c1fe8471d2ab
RafaAudibert <rafaeelaudibert@gmail.com>
2020-12-19 11:07:24 -0300

    :rocket: Add day 19

--- lib/advent_of_code/day_19.ex
+++ lib/advent_of_code/day_19.ex
@@ -1,15 +1,96 @@
 import AdventOfCode.Helpers.Input
 
 defmodule AdventOfCode.Day19 do
+  defp parse_lines_to_rules(lines_of_rules) do
+    Enum.map(lines_of_rules, fn line ->
+      [number, rule] = String.split(line, ": ", parts: 2)
+      {String.to_integer(number), rule}
+    end)
+    |> Enum.into(%{})
+  end
+
+  defp rules_to_list(rule) do
+    String.split(rule, " | ")
+    |> Enum.map(fn follow_rules ->
+      String.split(follow_rules, " ") |> Enum.map(&String.to_integer/1)
+    end)
+  end
+
+  defp expand_rules(rules, idx, memory \\ %{}) do
+    if memory[idx] != nil do
+      {memory[idx], memory}
+    else
+      curr_rule = rules[idx]
+
+      {result, memory} =
+        if Regex.match?(~r/\"[a-z]\"/, curr_rule) do
+          {[String.graphemes(curr_rule) |> Enum.at(1)], memory}
+        else
+          rules_for_curr = rules_to_list(curr_rule)
+
+          Enum.reduce(rules_for_curr, {[], memory}, fn list, {past_rules, memory} ->
+            {generated, memory} =
+              Enum.reduce(list, {[""], memory}, fn rule_to_follow, {curr_words, memory} ->
+                {generated, memory} = expand_rules(rules, rule_to_follow, memory)
+
+                {
+                  Enum.flat_map(curr_words, fn word -> Enum.map(generated, &(word <> &1)) end),
+                  memory
+                }
+              end)
+
+            {past_rules ++ generated, memory}
+          end)
+        end
+
+      {result, Map.put(memory, idx, result)}
+    end
+  end
+
+  defp consume(message, rules, count \\ 0) do
+    rule = Enum.find(rules, &String.starts_with?(message, &1))
+
+    if rule == nil do
+      {message, count}
+    else
+      rest = String.slice(message, byte_size(rule), byte_size(message))
+      consume(rest, rules, count + 1)
+    end
+  end
+
   @spec part1(String.t()) :: non_neg_integer()
   def part1(filename) do
-    _input = read_integers(filename)
-    0
+    [lines_of_rules, messages] = read_chunked_lines(filename)
+    rules = parse_lines_to_rules(lines_of_rules)
+
+    # We can do the following, because if we expand the 0 rule we have 42 42 31
+    [rules_42, rules_31] =
+      Enum.map([42, 31], &(rules |> expand_rules(&1) |> elem(0) |> MapSet.new()))
+
+    Enum.count(messages, fn message ->
+      {rest_message, count_42} = consume(message, rules_42)
+      {rest_message, count_31} = consume(rest_message, rules_31)
+
+      count_42 == 2 and count_31 == 1 and rest_message == ""
+    end)
   end
 
   @spec part2(String.t()) :: non_neg_integer()
   def part2(filename) do
-    _input = read_integers(filename)
-    0
+    [lines_of_rules, messages] = read_chunked_lines(filename)
+    rules = parse_lines_to_rules(lines_of_rules)
+
+    # We can do the following, because if we expand the 0 rule we have
+    # 42 42+ 31+, this is, n rules 42 followed by m rules 31, where n and m are not 0
+    # and m needs to be less than n
+    [rules_42, rules_31] =
+      Enum.map([42, 31], &(rules |> expand_rules(&1) |> elem(0) |> MapSet.new()))
+
+    Enum.count(messages, fn message ->
+      {rest_message, count_42} = consume(message, rules_42)
+      {rest_message, count_31} = consume(rest_message, rules_31)
+
+      count_42 >= 2 and count_31 > 0 and count_31 < count_42 and rest_message == ""
+    end)
   end
 end
--- test/advent_of_code/day_19_test.exs
+++ test/advent_of_code/day_19_test.exs
@@ -1,15 +1,13 @@
 defmodule AdventOfCode.Day19Test do
   use ExUnit.Case
-  
-  @tag :skip
+
   test "part1" do
     result = AdventOfCode.Day19.part1(AdventOfCode.Helpers.File.get_path(19))
-    assert result
+    assert result === 241
   end
 
-  @tag :skip
   test "part2" do
     result = AdventOfCode.Day19.part2(AdventOfCode.Helpers.File.get_path(19))
-    assert result
+    assert result === 424
   end
 end
