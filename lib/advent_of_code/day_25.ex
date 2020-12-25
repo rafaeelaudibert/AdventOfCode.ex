7e0efaeaaf19e88f3983eef6883cf09756f2da60
RafaAudibert <rafaeelaudibert@gmail.com>
2020-12-25 05:58:21 -0300

    :rocket:🎄 Added day 25

--- lib/advent_of_code/day_25.ex
+++ lib/advent_of_code/day_25.ex
@@ -3,13 +3,22 @@ import AdventOfCode.Helpers.Input
 defmodule AdventOfCode.Day25 do
   @spec part1(String.t()) :: non_neg_integer()
   def part1(filename) do
-    _input = read_integers(filename)
-    0
+    modulo = 20_201_227
+    subject_number = 7
+
+    [card_public, door_public] = read_integers(filename)
+
+    loop_size_card =
+      Stream.iterate(1, fn value -> rem(value * subject_number, modulo) end)
+      |> Enum.take_while(fn val -> val != card_public end)
+      |> length()
+
+    Stream.iterate(1, fn value -> rem(value * door_public, modulo) end)
+    |> Enum.take(loop_size_card + 1)
+    |> List.last()
   end
 
+  # Day 25 has no part 2
   @spec part2(String.t()) :: non_neg_integer()
-  def part2(filename) do
-    _input = read_integers(filename)
-    0
-  end
+  def part2(_), do: 2020
 end
--- test/advent_of_code/day_25_test.exs
+++ test/advent_of_code/day_25_test.exs
@@ -1,15 +1,13 @@
 defmodule AdventOfCode.Day25Test do
   use ExUnit.Case
-  
-  @tag :skip
+
   test "part1" do
     result = AdventOfCode.Day25.part1(AdventOfCode.Helpers.File.get_path(25))
-    assert result
+    assert result === 17_980_581
   end
 
-  @tag :skip
   test "part2" do
     result = AdventOfCode.Day25.part2(AdventOfCode.Helpers.File.get_path(25))
-    assert result
+    assert result === 2020
   end
 end
